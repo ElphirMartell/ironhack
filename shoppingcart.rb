5233b79ba465038a891d6430302661400a56fe7c
SRoa <sergioroa2@gmail.com>
2015-11-01 01:52:01 +0100

    product stocks created

--- shoppingcart.rb
+++ shoppingcart.rb
@@ -87,47 +87,46 @@ class ShoppingCart
 end
 
 class Shop
-    attr_reader :items
+attr_reader :item, :stock, :fruit_stock
     def initialize
-        @fruit_stock = []
-        @rice_stock = []
-        @vacuumcleaner_stock = []
-        @anchovies_stock = []
+        @stock = []
+
     end
 
-    def add_fruit(item)
-        @fruit_stock.push(item)
+=begin si quiero que funcione subiendo montones de productos tendré que crear un array fuera
+    def add_products(items)
+        items.each do |item|
+        @stock << item
+        end
+    end
+=end
+    def add_product(item)
+        @stock << item
     end
 
-    def add_rice(item)
-        @rice_stock.push(item)
-    end
-
-    def add_vacuum(item)
-        @vacuumcleaner_stock.push(item)
-    end
-
-    def add_anchovies(item)
-        @anchovies_stock.push(item)
-    end
-
-    def stock_panel
-        puts "There are #{@fruit_stock.size} units of orange juice and bananas in stock"
-        puts "There are #{@rice_stock.size} rice packets in stock"
-        puts "There are #{@vacuumcleaner_stock.size} vacuum cleaner units in stock"
-        puts "There are #{@anchovies_stock.size} units of anchovies in stock"
+    def view_stock
+        @fruit_stock = @stock.select{|item| item.class == Fruit}
+        puts "There are #{@fruit_stock.length} units of orange juice and bananas in stock"
+        @rice_stock = @stock.select{|item| item.class == Rice}
+        puts "There are #{@rice_stock.length} rice packets in stock"
+        @vacuumcleaner_stock = @stock.select{|item| item.class == VacuumCleaner}
+        puts "There are #{@vacuumcleaner_stock.length} vacuum cleaner units in stock"
+        @anchovies_stock = @stock.select{|item| item.class == Anchovies}
+        puts "There are #{@anchovies_stock.length} units of anchovies in stock"  
     end
 
 end
 
-madrid = Shop.new
-5.times{madrid.add_fruit(Fruit.new)}
-12.times{madrid.add_fruit(Fruit.new)}
-20.times{madrid.add_rice(Rice.new)}
-5.times{madrid.add_vacuum(VacuumCleaner.new)}
-17.times{madrid.add_anchovies(Anchovies.new)}
 
-puts madrid.stock_panel
+madrid = Shop.new
+5.times{madrid.add_product(Fruit.new)}
+12.times{madrid.add_product(Fruit.new)}
+20.times{madrid.add_product(Rice.new)}
+madrid.add_product(VacuumCleaner.new)
+17.times{madrid.add_product(Anchovies.new)}
+
+puts madrid.view_stock
+puts "Your stock is currently #{madrid.stock.length}"
 
 my_cart = ShoppingCart.new
 5.times {my_cart.add_items(Fruit.new)}
@@ -136,3 +135,5 @@ my_cart.add_items(VacuumCleaner.new)
 13.times {my_cart.add_items(Anchovies.new)}
 
 puts my_cart.calculate_total
+
+
